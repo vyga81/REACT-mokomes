367d6d9537cf0cccd1aba3c28701c0da8353ba7b
Vygantas Kazakevicius <104910000+vyga81@users.noreply.github.com>
2022-08-04 14:20:40 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './osquery.svg';
 import './App.css';
 
 function App() {
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Sveiki atvyke i REACT pasauli
+
         </p>
         <a
           className="App-link"
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          BIT jus kviecia
         </a>
       </header>
     </div>
